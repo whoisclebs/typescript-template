6cc0a715431c9aaad292050833ff3929c675218c
clebsonf <clebson.augusto@dcx.ufpb.br>
2022-10-03 18:03:29 -0300

    chore: a simple typescript template

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
